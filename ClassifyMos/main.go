63d3c09ccd5020ca354f512d3c21ac2e9d609544
Adam P <adam.plazewski@gmail.com>
2019-08-06 21:07:19 -0600

    pre-storing classification component/type -> name relationships

--- ClassifyMos/main.go
+++ ClassifyMos/main.go
@@ -13,7 +13,8 @@ func main() {
 
 	var existing_mos ManufactureOrder.Collection
 	var new_mos ManufactureOrder.Collection
-	var mo_classifier ManufactureOrder.Classifier
+	
+	mo_classifier := ManufactureOrder.NewClassifier()
 
 
 	// Load older MO data from json file 
@@ -22,18 +23,26 @@ func main() {
 	// Load MO data from Csv file
 	new_mos.LoadFromCsvFile(mos_csv_file_location)
 
-	// Import any new MOs from csv data into existing data
-	existing_mos.ImportOtherCollection(new_mos)
-
-	// Save updated data back to json file.
-	existing_mos.SaveToJsonFile(mos_json_file_location)
-
 	// Load Classifications
 	mo_classifier.LoadClassificationsFromFolder(classifications_folder)
 
+	// Create template classification file if none exist
 	if len(mo_classifier.Classifications) < 1 {
 		ManufactureOrder.GenerateClassificationTemplateInFolder(classifications_folder)
 	}
 
-	fmt.Println(mo_classifier)
+	// Import any new MOs from csv data into existing data
+	existing_mos.ImportOtherCollection(new_mos)
+
+	// Loop through each MO, classifying each one.
+	// for mo_key, mo := range existing_mos.Mos {
+	// 	fmt.Println(mo_key, mo_classifier.ClassifyMo(mo))
+	// }
+
+	// Save updated data back to json file.
+	existing_mos.SaveToJsonFile(mos_json_file_location)
+
+
+	mo_classifier.Display("")
+	fmt.Println()
 }
